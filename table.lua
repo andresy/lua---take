4e2029848cb972d6a7b8bf7ac2034e9a12582c23
Ronan Collobert <ronan@collobert.com>
2012-11-14 17:00:15 +0100

    make sure imerge returns uniq values

--- table.lua
+++ table.lua
@@ -13,6 +13,22 @@ function table.imerge(...)
          end
       end
    end
+
+   -- remove duplicate values
+   local uniq = {}
+   local k = 0
+   for _,v in ipairs(tbl) do
+      if not uniq[v] then
+         k = k + 1
+         uniq[v] = k
+      end
+   end
+
+   tbl = {}
+   for v,k in pairs(uniq) do
+      tbl[k] = v
+   end
+
    return tbl
 end
 
